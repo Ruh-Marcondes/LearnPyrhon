5041a08042389d2d1f5b8cfb56558b18b98d396f
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-06-05 21:15:19 -0300

    Resumo das videos aulas

---
+++ Aula/Aula1.py
@@ -0,0 +1,22 @@
+# Toda função tem ()
+# Números[usados em operacoes ] não estão em "", mensagens sim
+#Exempolo 
+
+a = "Hello "
+b = "Word!"
+print (a + b) # print = Console.WriteLine();
+print("7 + 8 = " , 7 + 8) # neste caso  7 + 8 sem ""
+#é usado para uma operação lógica 
+
+#Pode usar tanto '+' quanto ',' para se juntar mensagens
+
+#utilize sempre letras minusculas para váriaveis
+# váriaveis é um objeto... pro Python
+
+#Interação com usuaio
+
+nome = input(" Qual seu nome?\n")
+idade = input(" Olá "+ nome + ". \n Digite sua idade: ")
+peso  = input(" Agora digite seu peso: ")
+altura = input(" Digite, por favor, " + nome + " sua altura: ")
+#OBS: se o () tiver afastado da última letra da erro
---
+++ Aula/Aula2.py
@@ -0,0 +1,18 @@
+#tipos primitivos
+# a comunidade recomenda o uso se '' para Stings
+ 
+ # existe int
+ # floot
+ # boll - True False (valor lógico sempre a primeira em Maiúsculo)
+ # str  - String, txt como 'cry' '7.5' '' - string vazia
+ #Operação Lógica
+
+n1 = int (input('Pleas... \n I need much this... \n  Enter a number \n'))
+n2 = int (input('Pleas... \n I need much this... \n  Enter another number \n'))   
+s = n1 + n2 
+print('The sum of numbers is {}'.format(s))
+
+print("\n")
+n = float(input('Number....\n'))
+print(n)
+
