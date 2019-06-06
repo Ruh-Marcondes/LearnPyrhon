49e335ce2714b38a009876bfe85b15068b746027
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-06-05 21:47:37 -0300

    Exes

---
+++ Exercicio/M1/Ex3.py
@@ -0,0 +1,7 @@
+#Volume
+r = float(input("Digite o  raio da garrafa \n"))
+h = float(input("Digite a altura da garrafa \n"))
+vol = ((r**2)*3.14*h)  
+# ** - representa potenciação
+#   
+print("Volume = {}".format(vol))
---
+++ Exercicio/M1/Ex5.py
@@ -0,0 +1,4 @@
+#culto total
+nr = int(input("Número de aves a comprar: \n"))
+cus = (((nr*0.30)/15) + 10)
+print("Custuto: {}".format(cus))
