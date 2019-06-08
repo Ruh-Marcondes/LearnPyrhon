ecf2be3d454331011deaac1caf5fcc356aeccff0
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-06-08 18:04:53 -0300

    If melhorado
    Só algumas alterações

--- Aula/Aula3.py
+++ Aula/Aula3.py
@@ -2,58 +2,82 @@
 # Simples e Compostas 
 # If Else
 # progrmas não são bipolares... Como eu...
-import os
+from os import system
 import time
-os.system('echo.')
-n = str(input('Digite teu nome? \n '))
-if n == 'Ruahma':
-    time.sleep(0.5)
+
+def carinha():
     print(' \'-\' '*5)
     print('')
-    print(' Hello, {}, Bem - Vinda!'.format(n))
+
+
+def carinha2():
     print('')
     print(' \'-\' '*5)
-else:
-    print('Hello, {}'.format(n))
 
-time.sleep(3)
-os.system('cls')
 
-r = str(input(' Deseja continuar ? \n escolha s pra sim e n para não: '))
-if r == 's':
+def tracinho():
     print('')
-    print(' Calculo de média \n obs: -\'Como se nunca tivesse feito esse exe\' ')
-    print('')
-    no = input(' Qual o nome do aluno: ')
-    print('')
-    n1 = float(input(' Digite a primeira nota do {}: '.format(no)))
-    print('')
-    n2 = float(input(' Digite a segunda nota do {}: '.format(no)))
-    m = (n1+n2)/2
+    print('-'*15)
+
+def hello():
+    n = str(input('Digite teu nome? \n '))
+    if n == 'Ruahma':
+        time.sleep(0.5)
+        carinha()
+        print(' Hello, {}, Bem - Vinda!'.format(n))
+        carinha2()
+    else:
+        print('Hello, {}'.format(n))
+
+
+def media():
+    r = str(input(' Deseja continuar ? \n escolha s pra sim e n para não: '))
+    if r == 's':
+        print('')
+        print(' Calculo de média \n obs: -\'Como se nunca tivesse feito esse exe\' ')
+        print('')
+        no = input(' Qual o nome do aluno: ')
+        print('')
+        n1 = float(input(' Digite a primeira nota do {}: '.format(no)))
+        print('')
+        n2 = float(input(' Digite a segunda nota do {}: '.format(no)))
+        m = (n1+n2)/2
     if m>= 6.0:
-
         print(' Aprovado')
     else:
         print(' Reprovado')
         time.sleep(0.5)
-        print('')
-        print('-'*15)
-        print('')
+        tracinho()
+
+
+print('')
+hello()
+time.sleep(3)
+system('cls')
+print('')
+while True:
+    print(' OPC: ')
+    p = int(input(' 1...Hello \n 2... Media 3.. Sair'))
+    if p != 1 or p != 2 or p != 3:
+        print(' Erro! \n Escolha outra opção')
+    elif p == 1:
+        hello()
+    elif p == 3:
+        media()
+    else:
+        break
     r = str(input(' Deseja continuar ? \n escolha s pra sim e n para não: '))
     if r == 's': 
         print('')
-        os.system('cls')
+        system('cls')
         print('...Continuando')
         time.sleep(3)
         print(' Sorry I\'m tired' )
-        print(' Bey - bey \'-\' ') 
+        print(' Bey - bey \'-\' ')
+        break 
     else:
         time.sleep(0.5)
         print(' Tchau \'-\' ')
         time.sleep(1)
-        os.system('cls')  
-else:
-    time.sleep(0.5)
-    print(' Tchau \'-\' ')
-    time.sleep(1)
-    os.system('cls')    
+        system('cls')
+        break
