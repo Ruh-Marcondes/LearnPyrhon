581b400a74d1493ef44e5381975db017ccdb6402
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-06-08 17:34:24 -0300

    <3

--- Exercicio/M1/Ex7.py
+++ Exercicio/M1/Ex7.py
@@ -7,7 +7,8 @@
 n = input("Digite o nome do funcionário: \n ")
 p = float(input("Digite qual o válor unitário da peça: \n "))
 q = int(input("Digite a quantidade vendida: \n "))
-t = (p*q)
+t = p*q
 # Não se pode ter váriaveis com letra maiúscula
-c = ((t*5)/100)
+c = (t*5)/100)
 print(" O total recibido é R$ {}. \n Para o fúnciorio {} a comissão será {}.".format(t,n,c))
+#fim do algoritimo
