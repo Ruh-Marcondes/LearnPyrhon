3f8813d49e5fc163c693000eb5c64ac85d5dea56
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-06-15 11:20:51 -0300

    tipos de dados

--- Basico/Aula/Aula2.py
+++ Basico/Aula/Aula2.py
@@ -1,11 +1,10 @@
 #tipos primitivos
 # a comunidade recomenda o uso se '' para Stings
- 
- # existe int
- # floot
- # boll - True False (valor lógico sempre a primeira em Maiúsculo)
- # str  - String, txt como 'cry' '7.5' '' - string vazia
- #Operação Lógica
+# existe int
+# floot
+# boll - True False (valor lógico sempre a primeira em Maiúsculo)
+# str  - String, txt como 'cry' '7.5' '' - string vazia
+#Operação Lógica
 
 n1 = int (input('Pleas... \n I need much this... \n  Enter a number \n'))
 n2 = int (input('Pleas... \n I need much this... \n  Enter another number \n'))   
