72b4be55f1b1b6094f34f2c45810b11643615af3
Ruahma Marcondes <marcondes.ruahma@gmail.com>
2019-06-24 17:13:52 -0300

    Test

--- Basico/Exercicio/M2/e7.py
+++ Basico/Exercicio/M2/e7.py
@@ -1,4 +1,13 @@
-#Teste apenas
-# >>> amei star Vs forças do mal 
-print("'-'"*100)
-print(' I love you <3')
+
+# lol
+# Me love you  
+# Star Vs as Forças do Mal é top
+print('I Love you....')
+print('>>> Te <3')
+'''
+I am a genus
+'''
+'''
+I wanted know if you love me, like I love You
+'''
+
