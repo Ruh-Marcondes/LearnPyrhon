439d100982fa9a628ff8cd534040d566ba7c3d25
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-08-08 13:34:45 -0300

    <#>

--- Basico/Exercicio/M2/e7.py
+++ Basico/Exercicio/M2/e7.py
@@ -1,8 +1,5 @@
 '''
-    Quase ñ lembro dos comandos de Python
-
-                                    '-'
-
+                  '-'
         Aula 13 Mundo 2
 '''
 for c in range(0,7):
@@ -14,5 +11,5 @@ for r in range(2,-8,-2):
 
 print(' ')
 for c in range(0,3):
-    n= int(input('Digite um número: (interiro ) \n  __'))
+    n =  [int(input('Digite um número: (interiro ) \n  __'))]
     print(' ')
