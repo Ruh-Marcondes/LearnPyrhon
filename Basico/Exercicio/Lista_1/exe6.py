b3f06fc593645c5fabb422328e618e7deed109bb
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-03 18:34:35 -0300

    Exer 6

--- Basico/Exercicio/Lista_1/exe6.py
+++ Basico/Exercicio/Lista_1/exe6.py
@@ -4,3 +4,10 @@ import os
      seu percentual de desconto. 
      Calcule e mostre o preço do produto após o desconto.
 '''
+
+n = (float(input('\tDigite o valor do produto: ')))
+per = (float(input('\tDigite o percentual de aumento do segunte modo ex--> 40% -- 0.40:\t')))
+u = (per*n)
+n = n-u
+os.system('cls')
+print("\tO novo preço do produto é {:.2f}".format(n))
