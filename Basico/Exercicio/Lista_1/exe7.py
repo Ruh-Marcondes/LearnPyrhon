5fb51c27e24703dddd4b692b4e1bd771a90aac3b
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-03 18:40:14 -0300

    Triângulo retângulo

--- Basico/Exercicio/Lista_1/exe7.py
+++ Basico/Exercicio/Lista_1/exe7.py
@@ -1,5 +1,12 @@
 import os
+import math 
 '''
- Desenvolva um algoritmo que receba os valores dos catetos de um triangulo, 
- calcule e mostre o valor da hipotenusa.
-''' 
+Desenvolva um algoritmo que receba os valores dos catetos de um triangulo, calcule
+e mostre o valor da hipotenusa (hipotenusa = raiz quadrada (cateto1+ cateto2).
+'''
+
+l1 = float(input(" Digite um cateto:\t "))
+l2 = float (input(" Digite o outro cateto:\t "))
+f = math.hypot(l1,l2)
+f =  math.floor(f)
+print(" Hipotenusa aproximadamente =  {}".format(f))
