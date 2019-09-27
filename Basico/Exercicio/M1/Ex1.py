0a92abc6161b6076bc31810578b18650d9d0a40e
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-27 13:26:54 -0300

    Idade

--- Basico/Exercicio/M1/Ex1.py
+++ Basico/Exercicio/M1/Ex1.py
@@ -1,5 +1,8 @@
 #idade
+import time
 atual = int(input("Desculpe ñ estou conseguindo ascessar os dados sobre o ano atual... Porfavor me informe: \n"))
+print("")
 ano = int(input("E em que ano você nasceu? \n"))
 idade = atual - ano
 print("Sua idade é: {}, caso já tenha feito aniversário se não {} ".format(idade,idade - 1))        
+time.sleep(2)
