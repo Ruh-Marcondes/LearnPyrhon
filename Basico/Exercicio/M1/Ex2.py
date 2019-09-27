bd21d4fb69f58d9a387148aa4c55d48e103006a5
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-27 13:27:22 -0300

    area retângulo

--- Basico/Exercicio/M1/Ex2.py
+++ Basico/Exercicio/M1/Ex2.py
@@ -1,6 +1,9 @@
-#Area retangulp
+#Area retangulo
+import time
 print("Vamos calcular a área de um retângulo: ")
+print("")
 l1 = float(input("Digite um lado do retângulo \n"))
 l2 = float(input("Digite o outro lado do retângulo \n"))
 a  = l1*l2
 print ("A área do retângulo é: {}".format(a))        
+time.sleep(2)
