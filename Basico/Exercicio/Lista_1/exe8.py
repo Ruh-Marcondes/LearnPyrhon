9d4970c919743a1858ee94c3f70bfa62d7481672
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-28 20:15:24 -0300

    Lista 1

--- Basico/Exercicio/Lista_1/exe8.py
+++ Basico/Exercicio/Lista_1/exe8.py
@@ -1,5 +1,6 @@
 import os
 import time
+
 '''
 Faça um algoritmo que receba o número de horas trabalhadas e o valor do salário
 mínimo. Calcule e mostre o salário a receber seguindo as regras abaixo:
@@ -9,7 +10,10 @@ valor da hora trabalhada
 c. O imposto equivale a 3% do salário bruto;
 d. O salário a receber equivale ao salário bruto menos o imposto.
 '''
+
+os.system('color 0d')
 h = (int(input('\tDigite o número de hr trabalhados: ')))
 sm = (float(input('\tDigite o valor do salário minímo:  ')))
 time.sleep(0.5)
 os.system('cls')
+
